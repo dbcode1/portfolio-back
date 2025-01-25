a9ad3bf9e4b584ee1ad8c7fced51c0ad4f5f3262
daniel brusky <dmbrusky@gmail.com>
2025-01-25 16:09:00 -0500

    port

--- index.js
+++ index.js
@@ -45,5 +45,8 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
